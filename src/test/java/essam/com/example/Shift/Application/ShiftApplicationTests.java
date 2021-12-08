580d2d74bc3b1a37537152bbc5d6d3bd6c99ba81
Mohamed Essam <messam@sabbar.com>
2021-12-08 15:23:32 +0300

    Initialize Shifts Application

---
+++ src/test/java/essam/com/example/Shift/Application/ShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package essam.com.example.Shift.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
